c7930894f79934a11780d51c203c69e6c3e31617
HarryCMather <94319888+HarryCMather@users.noreply.github.com>
2025-06-25 13:28:39 +0100

    Create MainViewModel.cs

---
+++ src/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Discord.Video.Trimmer.ViewModels;
+
+public partial class MainViewModel : ObservableObject
+{
+    // TODO: I need to replace this with an actual implementation,
+    // just trying to keep the PR small enough for now, hence placeholders:
+    [ObservableProperty]
+    private string sampleText = "Hello World!";
+}
