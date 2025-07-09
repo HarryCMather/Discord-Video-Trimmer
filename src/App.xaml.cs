383254d354b59ad4e39028313a8f8b2ad2098f00
HarryCMather <94319888+HarryCMather@users.noreply.github.com>
2025-07-09 12:19:14 +0100

    feat: Ensure supported platform on start-up

--- src/App.xaml.cs
+++ src/App.xaml.cs
@@ -7,8 +7,44 @@ public partial class App : Application
         InitializeComponent();
     }
 
-    protected override Window CreateWindow(IActivationState? activationState)
+    protected override Window CreateWindow(IActivationState activationState)
     {
-        return new Window(new AppShell());
+        bool isDeviceSupported = IsDevicePlatformSupported();
+        Window window = CreateWindow(isDeviceSupported);
+        return window;
+    }
+
+    private static bool IsDevicePlatformSupported()
+    {
+        return DeviceInfo.Platform == DevicePlatform.WinUI ||
+               DeviceInfo.Platform == DevicePlatform.MacCatalyst;
+    }
+
+    private static Window CreateWindow(bool isDeviceSupported)
+    {
+        Page page = isDeviceSupported ? new AppShell() : new ContentPage();
+        Window window = new(page);
+
+        if (!isDeviceSupported)
+        {
+            page.Loaded += async (_, _) =>
+            {
+                await page.DisplayAlert(title: "Unsupported Platform", 
+                                        message: "This application only supports Windows and MacCatalyst.",
+                                        cancel: "Exit");
+                
+                // Platform-specific ways of exiting the application:
+                #if ANDROID
+                    Android.OS.Process.KillProcess(Android.OS.Process.MyPid());
+                #elif IOS
+                    System.Diagnostics.Process.GetCurrentProcess().CloseMainWindow();
+                #endif
+                
+                // Platform-agnostic fallback way of exiting the application:
+                Environment.Exit(0);
+            };
+        }
+        
+        return window;
     }
 }
